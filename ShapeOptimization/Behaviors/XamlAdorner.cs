29c6bab1b0a44ec75016133ba8b405e8edd035c8
djetson <djetson@gmail.com>
2017-10-07 00:47:13 -0700

    Organized a bunch of stuff. Icons have been split up and moved into their own folder. Each file contains the definition for a single icon. Each icon file is merged into a primary Icons ResourceDictionary.  I similar reorganization was done for the Adorners. Both Adorners.xaml and Icons.xaml have been merged into the main App.xaml resource dictionary. This pattern should probably be followed going forward to ensure everything stays organized. Refactored and renamed the AttachAdorners behavior. It is now called XamlAdorner, since it has been designed from the ground up with the idea that it should follow all of the same patterns that other WPF Visuals do. At this point, it allows for Adorners to be defined exactly like any other panel. As well, they are defined within the element they are meant to adorn. So they are as close as they can get to just being a standard Adorner. To keep things well organized, reusable adorners may be defined in ResourceDictionaries and bound to the Content property as a StaticResource provided that they include the x:Shared="false" attribute in their root element. This allows each adorned element to instantiate it's own distinct copy of the Adorner resource.

--- ShapeOptimization/Behaviors/XamlAdorner.cs
+++ ShapeOptimization/Behaviors/XamlAdorner.cs
@@ -1,47 +1,31 @@
 ﻿using ShapeOptimization.Adorners;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Interactivity;
 using System.Windows.Markup;
 
 namespace ShapeOptimization.Behaviors
 {
-    [ContentProperty("AdornerContent")]
-    public class AttachAdorners : Behavior<UIElement>, INotifyPropertyChanged
+    [ContentProperty("Content")]
+    public class XamlAdorner : Behavior<UIElement>, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void NotifyPropertyChanged([CallerMemberName]string property = "")
-    => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
 
         //TODO: Change this to a collection so that multiple adorners may be added.
-        //private static readonly DependencyProperty AdornerProperty = DependencyProperty.Register("Adorner", typeof(AdornerMetadata), typeof(AttachAdorners));
-        //public AdornerMetadata Adorner
-        //{
-        //    get { return (AdornerMetadata)GetValue(AdornerProperty); }
-        //    set { SetValue(AdornerProperty, value); }
-        //}
-
-        private ContentControl _AdornerContent = new ContentControl();
-        public object AdornerContent
+        private static readonly DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(object), typeof(XamlAdorner));
+        public object Content
         {
-            get { return _AdornerContent.Content; }
-            set { _AdornerContent.Content = value; NotifyPropertyChanged(); }
+            get { return (object)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
         }
 
         private Adorner _Adorner;
-
         private AdornerLayer _AdornerLayer = null;
-
 
         protected override void OnAttached()
         {
@@ -53,7 +37,7 @@ namespace ShapeOptimization.Behaviors
         private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
         {
             _AdornerLayer = AdornerLayer.GetAdornerLayer(AssociatedObject);
-            _Adorner = new AdornerExtension(AssociatedObject, AdornerContent);
+            _Adorner = new AdornerExtension(AssociatedObject, Content);
             _AdornerLayer.Add(_Adorner);
         }
 
